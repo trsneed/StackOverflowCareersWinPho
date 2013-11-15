2b29882f664e38b294169f2e0ec6ebdc9b332001
Tim Sneed <trsneed@gmail.com>
2013-11-15 06:23:56 -0600

    First pass at search control

--- StackOverflowCareers/MainPage.xaml.cs
+++ StackOverflowCareers/MainPage.xaml.cs
@@ -22,8 +22,6 @@ namespace StackOverflowCareers
             // Set the data context of the LongListSelector control to the sample data
             DataContext = App.ViewModel;
 
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
         }
 
         // Load data for the ViewModel Items
@@ -43,26 +41,12 @@ namespace StackOverflowCareers
                 return;
 
             // Navigate to the new page
-            NavigationService.Navigate(new Uri("/DetailsPage.xaml?selectedItem=" + (JobPostingSelector.SelectedItem as ItemViewModel).ID, UriKind.Relative));
+            NavigationService.Navigate(new Uri("/DetailsPage.xaml?selectedItem=" + (JobPostingSelector.SelectedItem as JobPostingViewModel).JobPosting.OrderId, UriKind.Relative));
 
             // Reset selected item to null (no selection)
             JobPostingSelector.SelectedItem = null;
         }
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
 
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
     }
 }
---
+++ StackOverflowCareers/SearchControl.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace StackOverflowCareers
+{
+    public partial class SearchControl : UserControl
+    {
+        public SearchControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
