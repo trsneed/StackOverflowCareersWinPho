4fd93db4b8249b64ca05e4ec1d5e8780ae883d4c
Tim Sneed <trsneed@gmail.com>
2013-11-19 20:14:08 -0600

    Getting close on location work

---
+++ StackOverflowCareers/Core/LocationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowCareers.Core
+{
+    public class LocationService
+    {
+        private GeoCoordinateWatcher _watcher;
+
+        public LocationService(GeoCoordinateWatcher watcher)
+        {
+            _watcher = watcher;
+        }
+        
+    }
+}
